f17b0f514eb88a63a2dad47644a03ac00f793ba6
韦宇恒 <weiyuheng.0306@bytedance.com>
2023-02-01 14:02:52 -0500

    rustdemo

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,39 +1,40 @@
 use yew::prelude::*;
 
-enum Msg{
-    AddOne
+enum Msg {
+    AddOne,
 }
 
-struct CounterComponent{
+struct CounterComponent {
     count: i64,
 }
 
-impl Component for CounterComponent{
+impl Component for CounterComponent {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self{
-        Self {count: 0}
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            count: 0,
+        }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message){
-        match msg{
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
             Msg::AddOne => {
                 self.count += 1;
-                true;
+                true // re-render component
             }
         }
     }
 
-    fn view(&self, ctx : &Context<Self>) -> Html{
+    fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
-        html!{
-            <div class = "container">
-                <p>{self.count}</p>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{"+1"}</button>
+        html! {
+            <div class="container">
+                <p>{ self.count }</p>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
             </div>
         }
-        
     }
 }
 
